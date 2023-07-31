4468804ec55e94f74d19f97711fe030239902416
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2023-08-01 00:37:29 +0100

    Add files to Python Introduction

---
+++ Python-Programming-Basics/First-Steps-in-Python-Coding/01_USD_to_BGN.py
@@ -0,0 +1,8 @@
+# 1 USD = 1.79549 BGN
+
+USD_TO_BGN = 1.79549  # the uppercase holds a constant.
+usd = float(input())
+
+bgn = usd * USD_TO_BGN
+
+print(bgn)
---
+++ Python-Programming-Basics/First-Steps-in-Python-Coding/hello_softuni.py
@@ -0,0 +1,67 @@
+import math
+from math import floor, ceil
+
+
+
+# square_meters = float(input())
+# lv_per_meter = 7.61
+# discount = 0.18
+# final_sum = (square_meters * lv_per_meter) - ((square_meters * lv_per_meter) * discount)
+# discount_in_lev = (square_meters * lv_per_meter) * discount
+#
+# print(f"The final price is: {final_sum} lv.")
+# print(f"The discount is: {discount_in_lev} lv.")
+
+
+# dog_food_pack_number = int(input())
+# cat_food_pack_number = int(input())
+#
+# dog_food_cost = float(dog_food_pack_number*2.50)
+# cat_food_cost = float(cat_food_pack_number*4)
+#
+# print(str(dog_food_cost + cat_food_cost) + " lv.")
+
+
+# architect_name = input()
+# number_of_projects = int(input())
+# hours_needed = number_of_projects * 3
+#
+# print(f"The architect {architect_name} will need {hours_needed} hours to complete {number_of_projects} project/s.")
+
+
+# first_name = input()
+# last_name = input()
+# age = int(input())
+# town = input()
+#
+# print(f"You are {first_name} {last_name}, a {age} years old person from {town}.")
+
+'''
+name = input()
+print(name)
+age = float(input())
+print(age + 4)
+print(type(age))
+is_true = bool(input())
+print(is_true)
+'''
+
+# num1 = int(input())
+# num2 = int(input())
+# mult = num1 * num2
+# print(mult)
+
+# name = input("Insert your name:")
+# print(f"Hello, ", end = "")
+# print(f"{name}")
+# print(f"!")
+
+# print(floor(4.5856))
+# print(ceil(4.5856))
+
+# inches = int(input())
+# centimeters_inches_ratio = 2.54
+# centimeters = inches * centimeters_inches_ratio
+# print(centimeters)
+
+
