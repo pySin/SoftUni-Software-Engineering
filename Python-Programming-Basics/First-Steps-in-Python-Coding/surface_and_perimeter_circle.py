f727560d2a8652a6bb23c3112e35963987de4067
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2023-08-29 14:25:41 +0300

    Add files via upload

---
+++ Python-Programming-Basics/First-Steps-in-Python-Coding/surface_and_perimeter_circle.py
@@ -0,0 +1,14 @@
+# Surface and parameter of circle
+
+P = 3.14159
+
+r = float(input())
+# A = πr²
+surface = P*r**2
+
+# C = 2πr
+circumference = 2*P*r
+
+print(f"{surface:.2f}")
+print(f"{circumference:.2f}")
+
