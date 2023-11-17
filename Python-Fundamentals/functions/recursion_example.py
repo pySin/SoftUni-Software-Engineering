c4e11ee95fcefee77190329248daefe03085c726
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2023-11-17 01:31:01 +0100

    Add files via upload

---
+++ Python-Fundamentals/functions/recursion_example.py
@@ -0,0 +1,9 @@
+# Recursion Example
+
+def count_numbers(n):
+    if n <= 10:
+        print(n)
+        count_numbers(n + 1)
+
+
+count_numbers(1)
---
+++ Python-Fundamentals/functions/repeat_string.py
@@ -0,0 +1,8 @@
+# Repeat String
+
+repeat_string = lambda letters, repetitions: letters * repetitions
+
+this_letters = input()
+repeat = int(input())
+
+print(repeat_string(this_letters, repeat))
