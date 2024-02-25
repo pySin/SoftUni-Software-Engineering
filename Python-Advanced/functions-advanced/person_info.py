69444690a10d0810f594b3c372d7ae84c785ea8b
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-02-25 18:04:14 +0000

    Add files via upload

---
+++ Python-Advanced/functions-advanced/person_info.py
@@ -0,0 +1,9 @@
+# Person Info
+
+
+def get_info(name, age, town):
+    return f"This is {name} from {town} and he is {age} years old"
+
+
+print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))
+print(get_info(*["George", 20, "Sofia"]))
