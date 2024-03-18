483c27ad2c02cac80ddb78a76406a9607058308d
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-03-18 20:09:33 +0000

    Add files via upload

---
+++ Python-Advanced/modules/math_operations/core/operations.py
@@ -0,0 +1,27 @@
+def sum_nums(num, num2):
+    return num + num2
+
+
+def subtract(num, num2):
+    return num - num2
+
+
+def multiply(num, num2):
+    return num * num2
+
+
+def divide(num, num2):
+    try:
+        num / num2
+    except ZeroDivisionError:
+        return None
+    return num / num2
+
+
+def power(num, num2):
+    return num ** num2
+
+
+
+
+
