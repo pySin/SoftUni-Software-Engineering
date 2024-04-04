19a08e6d04091fade38debdd0dc6b36966c947e0
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-04-04 01:23:54 +0100

    Add files via upload

---
+++ Python-OOP/decorators/multiply.py
@@ -0,0 +1,18 @@
+# Multiply
+
+
+def multiply(times):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            result = function(*args, **kwargs)
+            return result * times
+        return wrapper
+    return decorator
+
+
+@multiply(6)
+def add_ten(number):
+    return number + 10
+
+
+print(add_ten(6))
