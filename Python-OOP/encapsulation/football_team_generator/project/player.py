5bc0ff92ed50b224e9b55370d3485718015b0608
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-04-06 01:48:00 +0100

    Add files via upload

---
+++ Python-OOP/encapsulation/football_team_generator/project/player.py
@@ -0,0 +1,22 @@
+# Player
+
+
+class Player:
+    def __init__(self, name: str, sprint: int, dribble: int, passing: int, shooting: int):
+        self.__name = name
+        self.__sprint = sprint
+        self.__dribble = dribble
+        self.__passing = passing
+        self.__shooting = shooting
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    def __str__(self):
+        return f"Player: {self.__name}\nSprint: {self.__sprint}\nDribble: {self.__dribble}\n" \
+               f"Passing: {self.__passing}\nShooting: {self.__shooting}"
---
+++ Python-OOP/encapsulation/football_team_generator/project/team.py
@@ -0,0 +1,23 @@
+from typing import List
+from project.player import Player
+
+
+class Team:
+    def __init__(self, name: str, rating:int):
+        self.__name = name
+        self.__rating = rating
+        self.__players: List[Player] = []
+
+    def add_player(self, player: Player):
+        if player in self.__players:
+            return f"Player {player.name} has already joined"
+        else:
+            self.__players.append(player)
+            return f"Player {player.name} joined team {self.__name}"
+
+    def remove_player(self, player_name):
+        for i in range(len(self.__players)):
+            if player_name == self.__players[i].name:
+                return self.__players.pop(i)
+        else:
+            return f"Player {player_name} not found"
