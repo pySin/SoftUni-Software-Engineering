04b449290692606c0fd6897c60b75edfb1fa1a6f
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2024-06-29 12:37:33 +0100

    Add files via upload

---
+++ Python-Programming-Basics/advanced-if-else/number_operations.py
@@ -0,0 +1,39 @@
+# Number Operations
+
+# ⦁	N1 - цяло число;
+# ⦁	N2 - цяло число;
+# ⦁	Оператор - един символ измежду: "+", "-", "*", "/", "%".
+
+n1 = int(input())
+n2 = int(input())
+operator = input()  # "+", "-", "*", "/", "%"
+even_odd = ""
+
+if operator == "+":
+    if (n1 + n2) % 2 == 0:
+        even_odd = "even"
+    else:
+        even_odd = "odd"
+    print(f"{n1} {operator} {n2} = {n1 + n2} - {even_odd}")
+elif operator == "-":
+    if (n1 - n2) % 2 == 0:
+        even_odd = "even"
+    else:
+        even_odd = "odd"
+    print(f"{n1} {operator} {n2} = {n1 - n2} - {even_odd}")
+elif operator == "*":
+    if (n1 * n2) % 2 == 0:
+        even_odd = "even"
+    else:
+        even_odd = "odd"
+    print(f"{n1} {operator} {n2} = {n1 * n2} - {even_odd}")
+elif operator == "/":
+    if n2 == 0:
+        print(f"Cannot divide {n1} by zero")
+    else:
+        print(f"{n1} / {n2} = {n1 / n2 :.2f}")
+elif operator == "%":
+    if n2 == 0:
+        print(f"Cannot divide {n1} by zero")
+    else:
+        print(f"{n1} % {n2} = {n1 % n2}")
